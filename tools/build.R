3dd41cd8864079fff0c1ebb83127a47fbdcd8a6a
Akira Terui <hanabi0111@gmail.com>
2026-08-06 14:41:32 -0400

    📝 build

--- tools/build.R
+++ tools/build.R
@@ -11,3 +11,19 @@ devtools::check(vignettes=FALSE)
 # check syntax ------------------------------------------------------------
 
 lintr::lint_package()
+
+
+# test call ---------------------------------------------------------------
+
+file.copy(
+  "src/graphela.cpp",
+  file.path(tempdir(), "graphela.cpp")
+)
+
+Rcpp::sourceCpp(file.path(tempdir(), "graphela.cpp"))
+
+beta <- matrix(1, 3, 3)
+diag(beta) <- 0
+alpha <- c(1, 1, 1)
+state <- c(1, 0, 1)
+energy(state = state, alpha = alpha, beta = beta)
