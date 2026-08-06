cd6c81925d61bc8f4abad4337cabce1d131f257f
Akira Terui <hanabi0111@gmail.com>
2026-08-06 17:54:36 -0400

    📝 stpd() function

--- src/graphela.cpp
+++ src/graphela.cpp
@@ -12,6 +12,50 @@ double energy(
     const arma::rowvec& alpha,
     const arma::mat& beta
 ) {
-  mat res = -state * alpha.t() - (state * (state * beta).t() ) / 2;
+  mat res = - state * alpha.t() - (state * (state * beta).t() ) / 2;
   return as_scalar(res);
+}
+
+// [[Rcpp::export]]
+arma::rowvec stpd(
+    const arma::rowvec& state,
+    const arma::rowvec& alpha,
+    const arma::mat& beta
+) {
+
+  // {Declare variables}
+  // eflip: vector of energy after a flip of species "i"
+  // sign: sign vector for a flip of species "i"
+  // emin: candidate energy minimum after flipping
+  // idx: species index that reduces the energy most
+  rowvec eflip;
+  rowvec sign;
+  double emin;
+  uword idx;
+
+  // {Initialize}
+  // s: temporary state vector - subject to updates
+  // e: temporary energy scalar - subject to updates
+  rowvec s = state;
+  double e = energy(state, alpha, beta);
+
+  // {Steepest descent}
+  while(true) {
+    sign = (1 - 2 * s);
+    eflip = ((-alpha - s * beta) % sign) + e;
+    emin = eflip.min();
+
+    // break if no improvement in energy
+    if (emin >= e)
+      break;
+
+    // identify index to flip
+    idx = eflip.index_min();
+
+    // flip 0/1
+    s(idx) = 1 - s(idx);
+    e = emin;
+  }
+
+  return join_rows(s, rowvec({e}));
 }
