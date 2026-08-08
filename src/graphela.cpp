a4b819b2caef96573d4bb24d3f1e749ef63d0fc8
Akira Terui <hanabi0111@gmail.com>
2026-08-07 22:07:05 -0400

    📝 ridge(), initial commit

--- src/graphela.cpp
+++ src/graphela.cpp
@@ -87,3 +87,62 @@ arma::mat rss(
 
   return m;
 }
+
+// [[Rcpp::export]]
+arma::mat ridge(
+    arma::rowvec s0,
+    arma::rowvec s1,
+    arma::rowvec alpha,
+    arma::mat beta,
+    int n = 10000
+) {
+
+  // {Declare}
+  // flip: indices of species flipped (index)
+  // nf: number of flips, or steps from s0 to s1
+  // ns: number of species
+  // path: initial path sequence from s0 to s1, shuffled
+  // tip: tip state
+  uvec flip = find(abs(s0 - s1) == 1);
+  const int nf = flip.n_elem;
+  const int ns = alpha.n_elem;
+  uvec path = shuffle(flip);
+  rowvec tip(ns + 1);
+
+  // idx: index for swapping
+  // ms0: initialized matrix
+  // ms: matrix for intermediate states
+  // e: energy
+  uvec idx(2);
+  mat ms0(nf + 1, ns), ms(nf + 1, ns);
+  ms0.row(0) = s0;
+  vec e0(nf + 1), e(nf + 1);
+  e0(0) = energy(s0, alpha, beta);
+
+  for (int t = 0; t < n; ++t) {
+    // idx: indices for swap
+    idx = randperm(path.n_elem, 2);
+
+    // update path sequence by swapping indices
+    path.swap_rows(idx(0), idx(1));
+
+    // reset the initial state
+    ms = ms0;
+
+    // reset energy
+    e = e0;
+
+    // state sequence from s0 to s1
+    for (int i = 0; i < nf; ++i) {
+      // flip one species, update state
+      ms.row(i + 1) = ms.row(i);
+      ms(i + 1, path(i)) = 1 - ms(i + 1, path(i));
+
+      // energy of current state
+      e(i + 1) = energy(ms.row(i + 1), alpha, beta);
+    }
+
+  }
+
+  return join_rows(ms, e);
+}
